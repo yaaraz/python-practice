46a755db6ae092a0e2c21beb7c99bca631e63665
Yaara Zaslow <Yaara.zaslow@gmail.com>
2017-02-12 15:53:27 -0800

    uploads from Python practice

---
+++ Turtles.py
@@ -0,0 +1,30 @@
+import turtle # Allows us to use turtles
+def ch3Turtle_1():
+    wn = turtle.Screen() # Creates a playground for turtles
+    alex = turtle.Turtle() # Create a turtle, assign to alex
+
+    alex.forward(50) # Tell alex to move forward by 50 units
+    alex.left(90) # Tell alex to turn by 90 degrees
+    alex.forward(30) # Complete the second side of a rectangle
+
+    wn.mainloop() # Wait for user to close window
+
+ch3Turtle_1()
+import turtle
+wn = turtle.Screen()
+alex = turtle.Turtle()
+
+alex.forward(50)
+alex.left(90)
+alex.forward(30)
+    
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+
+wn.mainloop()
